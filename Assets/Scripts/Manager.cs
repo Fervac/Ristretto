b232edd3f0a0ae0cbbdc2818a406c65a50459572
Alexis BECHET <abechet@e1r5p14.42.fr>
2021-10-20 11:44:14 +0200

    Manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    #region SINGLETON PATTERN
+    public static Manager _instance;
+    public static Manager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<Manager>();
+
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("Manager");
+                    _instance = container.AddComponent<Manager>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        Screen.SetResolution(720, 1280, true);
+    }
+
+    public void Retry()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
