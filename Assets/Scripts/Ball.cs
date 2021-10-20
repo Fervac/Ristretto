5ff4a6c906f23d05c5043bc11d5ab45ed97507b1
Alexis BECHET <abechet@e1r5p14.42.fr>
2021-10-20 18:27:05 +0200

    Physic with raycast and reflect

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Vector2 direction;
+
+    private float rotation;
+    [SerializeField]
+    private Vector2 velocity;
+    private Vector2 previous;
+    private float speed;
+
+    private void Awake()
+    {
+        direction = Random.Range(0, 2) == 0 ? new Vector2(3, -3) : new Vector2(-3, -3);
+
+        //direction = new Vector2(3, -3);
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * Time.deltaTime) ;
+
+        velocity = ((transform.position - new Vector3(previous.x, previous.y, 0.0f))) / Time.deltaTime;
+        previous = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        FireRaycasts();
+    }
+
+    private void FireRaycasts()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+            {
+                print("There is a " + hit.collider.gameObject.name + " " + hit.distance +"m away from gameobject");
+
+                Debug.DrawLine(transform.position, hit.point);
+
+                //distanceToNext = hitInfo.distanceToNext;
+
+                if (hit.distance <= 0.5f)
+                {
+                    direction = Vector2.Reflect(velocity, hit.normal);
+
+                    if (hit.collider.gameObject.CompareTag("Brick"))
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                }
+
+                Vector2 tmp = Vector2.Reflect(velocity, hit.normal);
+                Debug.DrawLine(hit.point, hit.point + new Vector3(tmp.x, tmp.y, 0.0f));
+                    
+                //Debug.Log(direction);
+            }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float varTime;
+
+    private void Awake()
+    {
+        varTime = 3f;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        float speed = 10f;
+        float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.Translate(xMove, 0f, 0f);
+        // initially, the temporary vector should equal the player's position
+        Vector3 clampedPosition = transform.position;
+        // Now we can manipulte it to clamp the y element
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -7f, 7f);
+        // re-assigning the transform's position will clamp it
+        transform.position = clampedPosition;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
+
+    private void CheckInput()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetKey("space"))
+        {
+            
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            
+        }
+
+
+        if (Input.GetKeyDown("r"))
+        {
+            Manager.Instance.Retry();
+        }
+#endif
+
+#if UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                
+            }
+        }
+#endif
+    }
+
+
+    private void Coroutine()
+    {
+        //_boosting = true;
+        StartCoroutine(TestTimer());
+    }
+
+    IEnumerator TestTimer()
+    {
+        // a = velocity;
+        // velocity += boostSpeed;
+        // b = velocity;
+
+        yield return new WaitForSeconds(varTime);
+
+        //_decelerate = true;
+    }
+
+}
